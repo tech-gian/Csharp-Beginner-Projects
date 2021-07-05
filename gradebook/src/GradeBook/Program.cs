0e8797feac42974354046a8b8845c6b16a333eec
tech-gian <sdi1900059@di.uoa.gr>
2021-07-05 15:31:40 +0300

    Add, list of grades

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,39 +7,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            // var goes only with initialization
-            // var x = 5.6;
-            // double y;
-            // y = 4.8;
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            grades.Add(56.1);
 
-            // Initialization of arrays
-            // 1st way) double[] numbers = new double[3];
-            // 2nd way)
-            double[] numbers = new[] {12.7, 4.5, 3.2}; // else use new type_name[]
-            
-            // var result = numbers[0] + numbers[1] + numbers[2];
             var result = 0.0;
-            foreach (double number in numbers) {
+            foreach (double number in grades) {
                 result += number;
             }
-            
+            result /= grades.Count;
 
-            // Using List
-            List<double> grades = new List<double>(); // could be used var instead of double
-            grades.Add(56.1);
-            // foreach is used the same way
-            
-            
-            System.Console.WriteLine($"The average grade is {(result / numbers.Length):N3}");
-            // N3 means that is going to be displayed a number with 3 digits
-
-
-            if (args.Length > 0) {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else {
-                Console.WriteLine("Hello!");
-            }
+            System.Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
