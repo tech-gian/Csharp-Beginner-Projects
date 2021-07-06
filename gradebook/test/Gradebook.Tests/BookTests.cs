aca9237aa184f2d78b7bc350ae9f55b5683b1654
tech-gian <sdi1900059@di.uoa.gr>
2021-07-06 09:51:46 +0300

    Initial, tests

---
+++ gradebook/test/Gradebook.Tests/BookTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Gradebook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange
+            
+
+            // act
+            
+            
+            // assert
+
+
+        }
+    }
+}
