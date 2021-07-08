7151db0e1f5c5732d34ed1c0491197b8fe0dee31
tech-gian <sdi1900059@di.uoa.gr>
2021-07-08 16:47:41 +0300

    Add, Features

---
+++ LinqSamples/Features/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Features
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ LinqSamples/Features/MyLinq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Features.Linq
+{
+    public static class MyLinq
+    {
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+            foreach (var item in sequence)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
