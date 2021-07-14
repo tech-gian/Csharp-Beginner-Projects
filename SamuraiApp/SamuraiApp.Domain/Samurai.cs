18d44d3c35678f5de54e8cd27ec3218154f0d7b3
tech-gian <sdi1900059@di.uoa.gr>
2021-07-14 17:04:28 +0300

    Initial, samurai domain

---
+++ SamuraiApp/SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; }
+        //public int BattleId { get; set; }
+        public List<Samurai> SamuraiBattle { get; set; }
+        public SecretIdentity SecretIdentity { get; set; }
+
+        public Samurai()
+        {
+            Quotes = new List<Quote>();
+        }
+    }
+}
