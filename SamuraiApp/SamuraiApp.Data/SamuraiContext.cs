5279ddfc57a6d0753f94417419acf548ba2b42c0
tech-gian <sdi1900059@di.uoa.gr>
2021-07-14 17:04:07 +0300

    Initial, samurai data

---
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext: DbContext
+    {
+        //public static readonly LoggerFactory MyConsoleLoggerFactory
+        //    = new LoggerFactory(new[]
+        //    {
+        //        new ConsoleLoggerProvider((category, level)
+        //            => category == DbLoggerCategory.Database.Command.Name
+        //        && level == LogLevel.Information, true)
+        //    });
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                //.UseLoggerFactory(MyConsoleLoggerFactory)
+                .UseSqlServer(
+                "Server = (localdb)\\MSSQLLocalDB; Database = SamuraiAppData; Trusted_Connection = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>()
+                .HasKey(s => new { s.SamuraiId, s.BattleId });
+        }
+    }
+}
